0e5a53e800e69b60c2578fab0bfd35c09f5789bf
rian144hz <rianmatheus669@gmail.com>
2026-05-27 12:27:53 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/org/first_springboot/first_spriingboot/entities/User.java
+++ src/main/java/org/first_springboot/first_spriingboot/entities/User.java
@@ -3,6 +3,8 @@ package org.first_springboot.first_spriingboot.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
@@ -14,6 +16,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders  = new ArrayList<>();
 
     public User(){
     }
@@ -30,21 +35,35 @@ public class User implements Serializable {
         return id;
     }
 
+    public void setId(Long id) {this.id = id;}
+
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {this.name = name;}
+
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {this.email = email;}
+
+
     public String getPhone() {
         return phone;
     }
 
+    public void setPhone(String phone) {this.phone = phone;}
+
     public String getPassword() {
         return password;
     }
+
+    public void setPassword(String password) {this.password = password;}
+
+    public List<Order> getOrders() {return orders;}
 
     @Override
     public boolean equals(Object o) {
@@ -57,4 +76,8 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
+
+
 }
