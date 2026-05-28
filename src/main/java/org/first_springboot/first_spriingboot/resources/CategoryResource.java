2586273730d8b311019d625d6fa62f5a3cbf8f70
rian144hz <rianmatheus669@gmail.com>
2026-05-28 10:33:54 -0300

    Create CategoryResource

---
+++ src/main/java/org/first_springboot/first_spriingboot/resources/CategoryResource.java
@@ -0,0 +1,34 @@
+package org.first_springboot.first_spriingboot.resources;
+
+import org.first_springboot.first_spriingboot.entities.Category;
+import org.first_springboot.first_spriingboot.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories" )
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category>  list = categoryService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category obj = categoryService.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+}
