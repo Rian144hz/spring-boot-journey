8a1f5716a454c3db33ddb0f3b56360161f8e16a7
rian144hz <rianmatheus669@gmail.com>
2026-06-04 11:30:38 -0300

    User update

--- src/main/java/org/first_springboot/first_spriingboot/resources/UserResource.java
+++ src/main/java/org/first_springboot/first_spriingboot/resources/UserResource.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
@@ -16,24 +18,32 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-      User obj = service.findById(id);
-      return ResponseEntity.ok(obj);
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok(obj);
     }
+
     @PostMapping
-    public ResponseEntity<User> insert( @RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
         return ResponseEntity.ok().body(obj);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
