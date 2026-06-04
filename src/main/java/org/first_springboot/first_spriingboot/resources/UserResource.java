a64b68171e144d84b70aad86bd451432cc419be4
rian144hz <rianmatheus669@gmail.com>
2026-06-04 11:03:10 -0300

    User insert

--- src/main/java/org/first_springboot/first_spriingboot/resources/UserResource.java
+++ src/main/java/org/first_springboot/first_spriingboot/resources/UserResource.java
@@ -4,10 +4,7 @@ import org.first_springboot.first_spriingboot.entities.User;
 import org.first_springboot.first_spriingboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,11 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
       User obj = service.findById(id);
       return ResponseEntity.ok(obj);
+    }
+    @PostMapping
+    public ResponseEntity<User> insert( @RequestBody User obj){
+        obj = service.insert(obj);
+        return ResponseEntity.ok().body(obj);
+
     }
 }
