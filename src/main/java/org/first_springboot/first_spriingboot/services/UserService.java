c3b906de354dc166e4e5b44044b71f2fb6526bb4
rian144hz <rianmatheus669@gmail.com>
2026-06-04 13:49:04 -0300

    Add delete exception

--- src/main/java/org/first_springboot/first_spriingboot/services/UserService.java
+++ src/main/java/org/first_springboot/first_spriingboot/services/UserService.java
@@ -4,8 +4,10 @@ import org.first_springboot.first_spriingboot.entities.User;
 import org.first_springboot.first_spriingboot.repositories.UserRepository;
 import org.first_springboot.first_spriingboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +30,17 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
+        try {
+            userRepository.deleteById(id);
+        } catch (org.springframework.dao.DataIntegrityViolationException e) {
+            throw new org.first_springboot.first_spriingboot.services.exceptions.ResourceNotFoundException(e.getMessage());
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 
     public User update( Long id, User obj){
